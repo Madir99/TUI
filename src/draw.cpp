9c89cfcaf6e4891e23ff10e195aee991e5485e88
Madir99 <63061979+Madir99@users.noreply.github.com>
2024-04-04 12:33:45 +0200

    Update draw.cpp

--- src/draw.cpp
+++ src/draw.cpp
@@ -26,32 +26,51 @@ void LineH(int X, int Y, int lenght) {
   }
 }
 
-//draw horizontal single line
+//draw horizontal single line line
 void LineHS(int X, int Y, int lenght) {
   LineH<'─'>(X, Y, lenght);
 }
 
-//draw horizontal double line
+//draw horizontal double line line
 void LineHD(int X, int Y, int lenght) {
   LineH<'═'>(X, Y, lenght);
 }
 
-//draw vertical single line
+//draw vertical single line line
 void LineVS(int X, int Y, int lenght) {
   LineV<'│'>(X, Y, lenght);
 }
 
-//draw vertical double line
+//draw vertical double line line
 void LineVS(int X, int Y, int lenght) {
   LineV<'║'>(X, Y, lenght);
 }
 
 //-------------------BOXES-------------------
 
-//Draw box
+//draw box
 //second vector must be bigger than fist one
 template<char LU, char RU, char LD, char RD, char VL, char HL>
 void Box(int X1, int Y1, int X2, int Y2) {
   printf("\x1B[%d;%dH%c", Y1, X1, LU);
-  
+  for (int i = 0; i < (X2-X1-2); i++) {
+    printf("%c", HL);
+  }
+  printf("%c\x1B[%d;%dH%c", RU, Y2, X1, LD);
+  for (int i = 0; i < (X2-X1-2); i++) {
+    printf("%c", HL);
+  }
+  printf("%c", RD);
+  LineV<VL>(X1, Y1+1, Y2-Y1-2);
+  LineV<VL>(X2, Y1+1, Y2-Y1-2);
+}
+
+//draw single line box
+vodi BoxS(int X1, int Y1, int X2, int Y2) {
+  Box<'┌', '┐', '└', '┘', '─', '│'>(X1, Y1, X2, Y2)
+}
+
+//draw double line box
+vodi BoxD(int X1, int Y1, int X2, int Y2) {
+  Box<'╔', '╗', '╚', '╝', '═', '║'>(X1, Y1, X2, Y2)
 }
